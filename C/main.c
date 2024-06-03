0237b30a81ce1cbd826db7e313041f339de420cd
Hector <hectorqrs@live.com>
2024-06-03 11:05:02 -0300

    Alterações finais no código C

--- C/main.c
+++ C/main.c
@@ -18,48 +18,55 @@ for(;;){
   scanf("%s",senha1);
 
   if(strcmp(senha, senha1) == 0 && strcmp(nome, nome1) == 0){
+  	system("clear");
     printf("Conectado com sucesso!");
-    printf("\nSeja bem-vindo");
+    printf("\nSeja bem-vindo\n\n");
 
     for(;;){
       int opc, i;
 
-      system("clear");
-      printf("\n\n*------------------*");
+      printf("*------------------*");
       printf("\n1-Trocar de Nome");
-      printf("\n2-trocar senha");
+      printf("\n2-Trocar senha");
       printf("\n3-Mostrar login");
       printf("\n4-Sair");
       printf("\n*------------------*\n\n");
-      printf("Selecione a opcao: ");
+      printf("Selecione a opção: ");
       scanf("%d",&opc);
       switch(opc){
         case 1:
-          printf("Digite seu novo Nome:");
+          printf("Digite seu novo nome: ");
           scanf ("%s",nome);
+		  printf("Nome alterado com sucesso\n");
+		  sleep(2);
+          system("clear");
         break;
         
         case 2:
-          printf("Nome");
-          printf("Digite sua3 nova senha");
+          printf("Digite sua nova senha: ");
           scanf ("%s",senha);
-          pause();
+		  printf("Nome alterado com sucesso\n");
+		  sleep(2);
           system("clear");
         break;
 
         case 3:
-          printf("Nome:%s\nSenha:%s",nome,senha);
-          scanf("%c",&vazio);
-                break;
-        
+		  printf("Nome: ");
+		  puts(nome);
+		  printf("Senha: ");
+		  puts(senha);
+		  sleep(3);
+          system("clear");
+		break;
+
         case 4:
-          printf("obrigado!!");
+          printf("Obrigado!!\n");
           return 0;
         break;
 
         default:
           printf("\nOpcao Errada tente novamente\n");
-          system("pause");
+		  sleep(3);
           system("clear");
             }
         }
