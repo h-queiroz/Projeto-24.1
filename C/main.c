c3a67b4702dd287d454c1bcd468da2e59adf4bc0
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2024-06-03 10:49:37 -0300

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,77 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
+#include <unistd.h>
 
 int main(void)
 {
-    
-    char nome[15] = {"test"};
-    char senha[15] = {"test"};
-    char nome1[15];
-    char senha1[15];
+  char vazio;
+  char nome[15] = {"aluno"};
+  char senha[15] = {"1234"};
+  char nome1[15];
+  char senha1[15];
+for(;;){
+  printf("Digite seu nome\n:");
+  scanf("%s",nome1);
 
+  printf("Digite sua senha\n:");
+  scanf("%s",senha1);
 
-    
-    printf("Digite seu nome\n:");
-    scanf("%s",nome1);
+  if(strcmp(senha, senha1) == 0 && strcmp(nome, nome1) == 0){
+    printf("Conectado com sucesso!");
+    printf("\nSeja bem-vindo");
 
-    
-    printf("Digite sua senha\n:");
-    scanf("%s",senha1);
+    for(;;){
+      int opc, i;
 
-    if(strcmp(senha, senha1) == 0 && strcmp(nome, nome1) == 0){
-        printf("Conectado com sucesso!");
+      system("clear");
+      printf("\n\n*------------------*");
+      printf("\n1-Trocar de Nome");
+      printf("\n2-trocar senha");
+      printf("\n3-Mostrar login");
+      printf("\n4-Sair");
+      printf("\n*------------------*\n\n");
+      printf("Selecione a opcao: ");
+      scanf("%d",&opc);
+      switch(opc){
+        case 1:
+          printf("Digite seu novo Nome:");
+          scanf ("%s",nome);
+        break;
         
-        for(;;){
-            int opc, i;
+        case 2:
+          printf("Nome");
+          printf("Digite sua3 nova senha");
+          scanf ("%s",senha);
+          pause();
+          system("clear");
+        break;
 
-            system("cls");
-            printf("\n\n*------------------*");
-            printf("\n1-Trocar de Nome");
-            printf("\n2-trocar senha");
-            printf("\n3-Mostrar login");
-            printf("\n4-Sair");
-            printf("\n*------------------*\n\n");
-            
-            printf("Selecione a opcao: ");
-            scanf("%d",&opc);
-            switch(opc){
-                case 1:
-                printf("Digite seu novo Nome:");
-                scanf ("%s",nome);
-                
+        case 3:
+          printf("Nome:%s\nSenha:%s",nome,senha);
+          scanf("%c",&vazio);
                 break;
-                
-                case 2:
-                printf("Nome");
-                    for(i=0;i<=15;i++){
-                    printf("%c\n",nome[i]);
-                    }
-                    system("pause");
-                    system("cls");
-                break;
+        
+        case 4:
+          printf("obrigado!!");
+          return 0;
+        break;
 
-                case 3:
-                printf("Digite sua nova senha");
-                scanf ("%s",senha);
-                break;
-            
-                case 4:
-                    printf("obrigado!!");
-                    return 0;
-                break;
-
-                default:
-                printf("\nOpcao Errada tente novamente\n");
-                system("pause");
-                system("cls");
+        default:
+          printf("\nOpcao Errada tente novamente\n");
+          system("pause");
+          system("clear");
             }
         }
-        
+
     }else{
-        printf("Senha errada");
-        
+        printf("Nome ou Senha errada!\n");
+        system("clear");
+
+
+
     }
+}
     return 0;
 }
